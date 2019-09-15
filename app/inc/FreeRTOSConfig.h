a78c7b79e4d6930ddd7b26ecbd3b1e71afbc40a5
navalprashar <naval.prashar@gmail.com>
2019-09-15 20:52:09 +0530

    [HUB-12][SETUP] Update MCU Configuration for HUB

--- app/inc/FreeRTOSConfig.h
+++ app/inc/FreeRTOSConfig.h
@@ -53,7 +53,7 @@ extern uint32_t SystemCoreClock;
 #define configTICK_RATE_HZ                ( ( TickType_t ) 1000 )
 #define configMIN_PRIORITIES              ( tskIDLE_PRIORITY )
 #define configMAX_PRIORITIES              ( 5 )
-#define configMINIMAL_STACK_SIZE          ( ( unsigned short ) 130 )
+#define configMINIMAL_STACK_SIZE          ( ( unsigned short ) 128 )
 #define configTOTAL_HEAP_SIZE             ( ( size_t ) ( 75 * 1024 ) )
 #define configMAX_TASK_NAME_LEN           ( 10 )
 #define configUSE_TRACE_FACILITY          0
